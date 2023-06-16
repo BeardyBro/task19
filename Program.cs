d58c801ff687fe9a26f71924252e44ac47d720f9
Andrey Vershinsky <vershinsky90@gmail.com>
2023-06-16 13:56:58 +0300

    task19

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Insert five-digit number");
+int num = Convert.ToInt32(Console.ReadLine());
+int num1, num2 = 0;
+int pal = num;
+while (num > 0)
+{
+    num1 = num%10;
+    num2 = (num2*10) + num1;
+    num = num/10;
+}
+if (pal == num2 && pal>10000 && pal<=100000)
+{
+    Console.WriteLine("It is palindome");
+}
+else
+{
+    Console.WriteLine("Nope, try again");
+}
